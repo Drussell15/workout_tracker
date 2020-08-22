2fe55e6060c14f6f3480f45296cb9a2673e79479
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-08-22 01:39:13 -0700

    package json
    added json file

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-//require express and mongoose
-
 const express = require("express");
-const mongoose = require("mongoose");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
 
-//sets up port and express app
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//static files go here
 app.use(express.static("public"));
 
-//connection to mongoose database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds015878.mlab.com:15878/heroku_7n1m7p7k 
+
+//setup routes//
+app.use(require("./routes/api-routes.js"));
+
+//html route//
+//homepage//
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+//exercist page display
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/exercise.html'));
+});
+//stat page//
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/stats.html'));
 });
 
-// routes
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
-
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`app running on port ${PORT}!`);
 });
