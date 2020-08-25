39008a11386e51204330c128314e632009f658ae
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-08-24 22:33:00 -0700

    making better
    made adjustment to server.js file

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
+//require express, morgan and mongoose
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+//setting up port and express app
+const PORT = process.env.PORT || 4000;
+
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//setting up static files
+const PORT = process.env.PORT || 4000;
+//get the goose going//
 
-app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds015878.mlab.com:15878/heroku_7n1m7p7k 
-
+});
 //setup routes//
 app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
-//html route//
-//homepage//
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-//exercist page display
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/exercise.html'));
-});
-//stat page//
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/stats.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}!`);
