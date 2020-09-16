cb0f8c49c36dafb3898580cac31b27e326c053c4
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-09-16 01:32:04 -0700

    Update server.js
    fixed routes, fixed mongodb connection

--- server.js
+++ server.js
@@ -4,26 +4,27 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 //setting up port and express app
-const PORT = process.env.PORT || 4000;
-
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //setting up static files
-const PORT = process.env.PORT || 4000;
-//get the goose going//
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, { 
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useFindAndModify: false
+})
 
-});
 //setup routes//
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+app.use("/api", require("./routes/api.js"));
+app.use("/", require("./routes/html.js"));
 
 
 app.listen(PORT, () => {
